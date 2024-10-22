d4f8aff5b2d09b475424726d43d285c6f419596b
martin joel lage <118794154+Martinjl2006@users.noreply.github.com>
2024-10-21 23:39:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector('.card').addEventListener('mouseover', function() {
+document.querySelector('#body').style.boxShadow = '10px 10px cyan';});
+document.querySelector('.card').addEventListener('mouseout', function() {
+document.querySelector('#body').style.boxShadow = '10px 10px rgba(18, 18, 18, 0.148)';});
